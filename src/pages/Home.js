ddf4522a5d3b384a280bf1519ad32c3a1a5064e3
f20160453 <f20160453@hyderabad.bits-pilani.ac.in>
2025-05-20 14:41:23 -0400

    Home page changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <main style={{ padding: '2rem'}}>
             <p>This site showcases my projects built and deployed</p>
-            <button onClick={() => alert=('Hello!')}>Click Me</button>
+            <button onClick={() => alert('Hello!')}>Click Me</button>
             <footer style={{ marginTop: '2rem', fontSize: '0.8rem'}}>
                 © {new Date().getFullYear()} Tanishk Deoghare
             </footer>
