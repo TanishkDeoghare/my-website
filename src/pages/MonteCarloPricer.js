078f11435f39c4582659a3a9e12b9c5728e393a8
f20160453 <f20160453@hyderabad.bits-pilani.ac.in>
2025-05-20 14:32:54 -0400

    monte carlo updated

--- src/pages/MonteCarloPricer.js
+++ src/pages/MonteCarloPricer.js
@@ -11,7 +11,7 @@ export default function MonteCarloPricer() {
 
     const handleSubmit = async e => {
         e.preventDefault();
-        const res = await fetch('/api/mc-price', {
+        const res = await fetch('http://localhost:5000/api/mc-price', {
             method : 'POST',
             headers: {'Content-Type':'application/json'},
             body: JSON.stringify(inputs),
