e61ef0d9b57b6ecbb9fd9e39d19b23711c221246
f20160453 <f20160453@hyderabad.bits-pilani.ac.in>
2025-05-21 09:51:34 -0400

    sidebar update

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,13 +5,18 @@ export default function Sidebar() {
         <nav className="sidebar">
             <ul>
                 <li><NavLink to="/">Home</NavLink></li>
-                <li><NavLink to="bs-pricer">Black-Scholes-Pricer</NavLink></li>
-                <li><NavLink to="mc-pricer">Monte-Carlo-Pricer</NavLink></li>
-                <li><NavLink to="black-pricer">Black's-model-pricer</NavLink></li>
-                <li><NavLink to="binomial-pricer">Binomial-model-pricer</NavLink></li>
-                <li><NavLink to="trinomial-pricer">Trinomial-model-pricer</NavLink></li>
-                <li><NavLink to="PDE-pricer">PDE-model-pricer</NavLink></li>
             </ul>
+            <div className="Sidebar section">
+                <h3>Option Pricers</h3>
+                <ul>
+                    <li><NavLink to="bs-pricer">Black-Scholes-Pricer</NavLink></li>
+                    <li><NavLink to="mc-pricer">Monte-Carlo-Pricer</NavLink></li>
+                    <li><NavLink to="black-pricer">Black's-Model-Pricer</NavLink></li>
+                    <li><NavLink to="binomial-pricer">Binomial-Model-Pricer</NavLink></li>
+                    <li><NavLink to="trinomial-pricer">Trinomial-Model-Pricer</NavLink></li>
+                    <li><NavLink to="PDE-pricer">PDE-Model-Pricer</NavLink></li>
+                </ul>
+            </div>
         </nav>
     );
 }
